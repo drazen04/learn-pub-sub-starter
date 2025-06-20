9f0ed0e732dbb8b19c6e68b8823ed4d02467caca
drazen04 <hangtime23@hotmail.it>
2025-06-20 16:48:57 +0200

    add amqp client and connect to rabbit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("could not connect to to RabbitMQ %v", err)
+	}
+	fmt.Println("Peril game server is connected to RabbitMQ!")
+	defer conn.Close()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChan
+	fmt.Println("Exiting from peril...")
 }
